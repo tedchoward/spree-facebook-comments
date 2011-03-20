3af3520fcf27ea94c18aaa11e05eadbef47bf4c1
Ted Howard <ted@tedchoward.com>
2011-03-20 16:01:22 -0500

    added facebook comments for each product page

--- lib/facebook_comments_hooks.rb
+++ lib/facebook_comments_hooks.rb
@@ -1,3 +1,9 @@
 class FacebookCommentsHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
+  insert_after :product_properties do
+    "<div class='product-fb-comment'>
+     <div id=\"fb-root\"></div>
+     <script src=\"http://connect.facebook.net/en_US/all.js#appId=<%= Spree::Config[:facebook_app_id] %>&amp;xfbml=1\"></script>
+     <fb:comments href=\"<%= url_for(:action => 'show', :controller => 'products', :id => @product.permalink, :only_path => false) %>\" num_posts=\"<%=Spree::Config[:facebook_comments_number]%>\" width=\"<%=Spree::Config[:facebook_comments_width]%>\"></fb:comments>
+     </div>"
+  end
 end
