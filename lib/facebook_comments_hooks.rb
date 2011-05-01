720a816100fddddd7acc136d74c633279d410052
Ted Howard <ted@tedchoward.com>
2011-04-30 22:11:01 -0500

    changed the comments box to the dark color scheme

--- lib/facebook_comments_hooks.rb
+++ lib/facebook_comments_hooks.rb
@@ -3,7 +3,7 @@ class FacebookCommentsHooks < Spree::ThemeSupport::HookListener
     "<div class='product-fb-comment'>
     <div id=\"fb-root\"></div>
     <script src=\"http://connect.facebook.net/en_US/all.js#appId=<%= Spree::Config[:facebook_app_id] %>&amp;xfbml=1\"></script>
-     <fb:comments href=\"<%= url_for(:action => 'show', :controller => 'products', :id => @product.permalink, :only_path => false) %>\" num_posts=\"<%=Spree::Config[:facebook_comments_number]%>\" width=\"<%=Spree::Config[:facebook_comments_width]%>\"></fb:comments>
+     <fb:comments href=\"<%= url_for(:action => 'show', :controller => 'products', :id => @product.permalink, :only_path => false) %>\" num_posts=\"<%=Spree::Config[:facebook_comments_number]%>\" width=\"<%=Spree::Config[:facebook_comments_width]%>\" colorscheme=\"dark\"></fb:comments>
      </div>"
   end
 
